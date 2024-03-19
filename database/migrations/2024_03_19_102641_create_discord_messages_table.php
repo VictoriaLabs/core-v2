3e8dce01237f104dbf068b3aa8c3a093aa3f8a36
GaetanccgYnov <gaetan.chollet@ynov.com>
2024-03-19 12:16:31 +0100

    initialisation des migrations pour toute la db

---
+++ database/migrations/2024_03_19_102641_create_discord_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiscordMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discord_messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('task_id');
+            $table->foreignId('discord_server_id')->constrained('discord_servers');
+            $table->string('channel_id');
+            $table->foreignId('user_id')->constrained('users');
+            $table->text('message');
+            $table->json('datas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discord_messages');
+    }
+}
---
+++ database/migrations/2024_03_19_110638_create_oauth_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOauthTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('oauth', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('oauth_services_id')->constrained('oauth_services');
+            $table->string('access_token');
+            $table->string('token_type');
+            $table->timestamp('expired_at');
+            $table->string('refresh_token');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('oauth');
+    }
+}
